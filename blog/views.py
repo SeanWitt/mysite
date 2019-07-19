ab629b4e4b31db78419f0d76c3b60d295c2ed785
Sean Witt <sean.robin.witt@gmail.com>
2019-07-18 22:02:50 -0500

    add template tags

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
+from taggit.models import Tag
+
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 
@@ -43,11 +46,16 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+    #Find Similar posts
+    post_tag_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tag_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
     return render(request,
            'blog/post/detail.html',
            {'post':post,
             'comments': comments, 
-            'comment_form': comment_form})
+            'comment_form': comment_form,
+            'similar_posts': similar_posts})
     
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status='published')
@@ -95,9 +103,11 @@ def post_list(request, tag_slug=None):
     object_list = Post.published.all()
     tag = None
     
+    
     if tag_slug:
-        tag = get_object_or_404(tag, slug=tag_slug)
-        object_list.filter(tags__in=[tag])
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+        
     
     paginator = Paginator(object_list, 3)
     page = request.GET.get('page')
